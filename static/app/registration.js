b5ee99bb22c2dc050430dd5608970094033b5e7d
FedorBozicRA99-2018 <74472316+FedorBozicRA99-2018@users.noreply.github.com>
2021-09-05 23:56:05 +0200

    Navbar (nefunkcionalan)

--- static/app/registration.js
+++ static/app/registration.js
@@ -6,28 +6,36 @@ Vue.component('registration', {
 		}
     },
     template: `
-		<div class="login-reg-panel">			
-			<div class="register-info-box">
-				<h2>Have an account?</h2>
-				<p>Click here to log in instead</p>
-				<label id="label-login" for="log-login-show">Log in</label>
-				<input type="radio" name="active-log-panel" id="log-login-show">
-			</div>
-								
-			<div class="white-panel">
-				<div class="register-show">
-					<h2>REGISTER</h2>
-					<input type="text" placeholder="Username" v-model="user.username" >
-					<input type="password" placeholder="Password" v-model="user.password" >
-					<div class="form-row">
-						<div class="col">
-						  <input type="text" placeholder="First Name" v-model="user.firstName" >
+    	<div>
+	    	<nav class="navMenu navbar-dark bg-dark">
+			    <a href="#" v-if="currentUser == null">Login</a>
+			    <a href="#" v-if="currentUser != null">Basket</a>
+			    <a href="#" v-if="currentUser != null">Profile</a>
+			    <a href="#" v-if="currentUser != null">Logout</a>
+		    </nav>
+			<div class="login-reg-panel">			
+				<div class="register-info-box">
+					<h2>Have an account?</h2>
+					<p>Click here to log in instead</p>
+					<label id="label-login" for="log-login-show">Log in</label>
+					<input type="radio" name="active-log-panel" id="log-login-show">
+				</div>
+									
+				<div class="white-panel">
+					<div class="register-show">
+						<h2>REGISTER</h2>
+						<input type="text" placeholder="Username" v-model="user.username" >
+						<input type="password" placeholder="Password" v-model="user.password" >
+						<div class="form-row">
+							<div class="col">
+							  <input type="text" placeholder="First Name" v-model="user.firstName" >
+							</div>
+							<div class="col">
+							  <input type="text" class="form_control" placeholder="Last Name" v-model="user.lastName" >
+							</div>
 						</div>
-						<div class="col">
-						  <input type="text" class="form_control" placeholder="Last Name" v-model="user.lastName" >
-						</div>
+						<input type="button" value="Register" v-on:click="register()"/>
 					</div>
-					<input type="button" value="Register" v-on:click="register()"/>
 				</div>
 			</div>
 		</div>
