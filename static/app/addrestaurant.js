07965fbc6a0451092f1ade9a85c80fce182065c9
FedorBozicRA99-2018 <74472316+FedorBozicRA99-2018@users.noreply.github.com>
2021-09-07 02:37:46 +0200

    Instantna promena slike

--- static/app/addrestaurant.js
+++ static/app/addrestaurant.js
@@ -17,7 +17,8 @@ Vue.component('addrestaurant', {
 	    		<article class="restaurant_card">
 				    <figure class="card-image" style="margin: 0px; padding: 0;">
 				    	<label for="image" style="margin: 0px; padding: 0; width:360px; height:202px;">
-						   <img src="/Add_Image.jpg" alt="" style="max-width:100%; max-height:100%;" />
+				    		<img v-if="image" :src="'data:image/png;base64,' + image" />
+							<img v-else src="/Add_Image.jpg" alt="" style="max-width:100%; max-height:100%;" />
 						 </label>
 						 <input type="file" v-on:change="convertImage" id="image" name="image" accept="image/*" style="display:none">
 				    </figure>
