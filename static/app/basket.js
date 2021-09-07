d7309ceb0c404736df9dfd57fb75faad24f4a27c
FedorBozicRA99-2018 <74472316+FedorBozicRA99-2018@users.noreply.github.com>
2021-09-08 00:12:44 +0200

    Izracunavanje totalne cene

--- static/app/basket.js
+++ static/app/basket.js
@@ -2,32 +2,46 @@ Vue.component('basket', {
     data: function () {
 		return {
 			cart: {},
-			currentUser: null
+			currentUser: null,
+			totalprice: 0
 		}
     },
     template: `
-	<div>
-		<div class="d-flex justify-content-center" style="margin-top:20px">
-	    	<div class="row row-cols-1 row-cols-md-2" style = "width: 85%;">
-		    	<table v-for="i in cart.cartItems" style="background-color: white"><!--:key="i.uuid"-->
-				  <tr>
-				  	<th>Naziv</th>
-				  	<th>Cena</th>
-				  	<th>Opis</th>
-				  	<th>Tip</th>
-				  	<th>Kolicina</th>
-				  	<th>Slika</th>
-				  </tr>
-				  <tr>
-				  </tr>
-				  	<td>{{i.item.name}}</td>
-				  	<td>{{i.item.price}}</td>
-				  	<td>{{i.item.description}}</td>
-				  	<td>{{i.item.type}}</td>
-				  	<td>{{i.count}}</td>
-				  	<td><img :src="i.item.image" alt="" /></td>
-				  </tr>
-				</table>
+	<div class="container">
+		<div class="row">
+		    <div class="container-fluid p-0" style="margin-top:100px">
+				<div class="row">
+					<div class="col-xl-8">
+						<table class="table table-striped" style="width:100%">
+							<thead style="background-image: linear-gradient(to right,rgba(236, 48, 20) 0%,rgba(250, 30, 20, 0.9) 100%); color:white">
+								<tr>
+									<th>Name</th>
+									<th>Amount</th>
+									<th>Price</th>
+								</tr>
+							</thead>
+							<tbody v-for="i in cart.cartItems">
+								<tr>
+									<td>{{i.item.name}}</td>
+									<td>{{i.count}}</td>
+									<td>{{i.item.price}}</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+					
+					<div class="col-xl-4">
+						<div class="card">
+							<div class="card-header">
+								<h5 class="card-title mb-0" style="text-align:left">Total</h5>
+								<h1 style="color:white; text-align:left"><strong>{{totalprice}}$</strong></h1>
+							</div>
+							<div class="card-body" style="background: -webkit-gradient(linear, left top, right top, from(darkgreen), to(forestgreen)); background: linear-gradient(to right, darkgreen, forestgreen); color:white">
+								<h4><strong>CHECKOUT</strong></h4>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -46,6 +60,7 @@ Vue.component('basket', {
 				
             	for(let ciId in this.cart.cartItems){
             		let ci = this.cart.cartItems[ciId];
+            		this.totalprice += (ci.count*ci.item.price)
             		if(ci.image != null){
             			ci.image = 'data:image/png;base64,' + ci.image;
             		}
