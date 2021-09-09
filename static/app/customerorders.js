dc86bab58aa8f0ac7d8091e43eb79ac6de6283ae
FedorBozicRA99-2018 <74472316+FedorBozicRA99-2018@users.noreply.github.com>
2021-09-09 06:00:48 +0200

    Mala ispravka

--- static/app/customerorders.js
+++ static/app/customerorders.js
@@ -7,9 +7,9 @@ Vue.component('customerorders', {
         },
         template: `
         <div class="container">
-		    <div class="container-fluid p-0" style="margin-top:100px">
-				<div class="row" v-for="order in orders">
-					<div class="card-header">{{order.restaurantName}}</div>
+		    <div class="container-fluid p-0" style="margin-top:100px" v-for="order in orders">
+				<div class="row"><div class="card-header">{{order.restaurantName}}</div></div>
+				<div class="row">
 					<div class="col-xl-8">
 						<table class="table table-striped" style="width:100%">
 							<thead style="background-image: linear-gradient(to right,rgba(236, 48, 20) 0%,rgba(250, 30, 20, 0.9) 100%); color:white">
@@ -21,7 +21,7 @@ Vue.component('customerorders', {
 							<tbody v-for="ci in order.items">
 								<tr>
 									<td>{{ci.item.name}}</td>
-									<td>{{ci.item.count}}</td>
+									<td>{{ci.count}}</td>
 								</tr>
 							</tbody>
 						</table>
